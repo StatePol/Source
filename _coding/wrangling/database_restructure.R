b503a1332a477e1b99adb53151e2fcb5e33dc98f
Daniel Kuhlen <danielkuhlen@MacBook-Pro-von-Daniel.local>
2023-09-25 17:42:47 +0200

    new structure

---
+++ _coding/wrangling/database_restructure.R
@@ -0,0 +1,47 @@
+
+# packages
+library(tidyverse)
+library(rio)
+
+# data import
+list <- import("_data/input/list.csv")
+cabinet <- import("_data/input/cabinet.csv")
+
+# mandate dataframe
+mandate <- list %>%
+  select(id, name.wikitag, constituency, mandate, voteshare, landtag.state,
+         landtag.state.abb, electoralperiod, term.start, term.end) %>%
+  mutate_if(is.character, ~na_if(., ''))
+  
+  
+# distinct dataframe for parliment
+legislative <- list %>%
+  rename(birthplace.latitude = birthplace.lat.geocode,
+         birthplace.longititude = birthplace.lon.geocode) %>% 
+  select(name, name.wikitag, birthplace, birthplace.wikitag,
+         birthplace.latitude, birthplace.longititude, birthplace.region,
+         birthplace.subregion, birthplace.country, birthplace.country.code,
+         birthplace.state, birthplace.state.code, birthplace.city, birthdate, gender) %>%
+  distinct(name.wikitag, .keep_all = T)
+
+# distinct dataframe for executive
+executive <- cabinet %>%
+  rename(birthplace.latitude = latitude,
+         birthplace.longititude = longitude) %>% 
+  select(name, name.wikitag, birthplace, birthplace.wikitag,
+         birthplace.latitude, birthplace.longititude, birthplace.region,
+         birthplace.subregion, birthplace.country, birthplace.country.code,
+         birthplace.state, birthplace.state.code, birthplace.city, birthdate, gender) %>%
+  distinct(name.wikitag, .keep_all = T)
+
+# bind_rows
+politicians <- bind_rows(legislative, executive) %>%
+  mutate_if(is.character, ~na_if(., '')) %>% 
+mutate_if(is.character, ~na_if(., 'NA')) %>% 
+  distinct(name.wikitag, .keep_all = T) %>%
+  arrange(name)
+
+# export
+export(mandate, "_data/output/mandate.csv")
+export(politicians, "_data/output/politicians.csv")
+
